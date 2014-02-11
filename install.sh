b05c50646c922342b36e77f159e48bb762e4e740
Samuele Santi <redshadow@hackzine.org>
2014-02-11 17:28:26 +0100

    Fixed dependency links for some versions of pip

--- install.sh
+++ install.sh
@@ -13,14 +13,16 @@ fi
 
 cd "$( dirname "$BASH_SOURCE" )"
 
+PIP_LOCAL="pip install --find-links file://${PWD}/dependencies/ --no-index"
+
 
 ## Install ckan + plugins + dependencies, only reading from
 ## the local packages archive.
-pip install --find-links ./dependencies/ --no-index -r sources/ckan/requirements.txt
+$PIP_LOCAL -r sources/ckan/requirements.txt
 
 ## If Python < 2.7, install ordereddict
 if python -c 'import sys;sys.exit(0 if sys.version_info < (2, 7) else 1)'; then
-    pip install --find-links ./dependencies/ --no-index ordereddict==1.1
+    $PIP_LOCAL ordereddict==1.1
 fi
 
 ## Install the application + plugins
